9dfc3f493f48b932a96a4096417d7f5daa30b45b
Luke <lukee249@outlook.com>
2023-09-18 15:02:18 -0500

    working in docker

---
+++ NoteToSelf.md
@@ -0,0 +1,4 @@
+# Using MERN, MongoDB, Express, React, Node.js
+# Supplemented with Docker in WSL2 for containerization and deployment
+# Vite for build and development
+
---
+++ react/src/App.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect, useRef } from 'react';
+import socketIOClient from "socket.io-client";
+
+const ENDPOINT = "http://localhost:8000";
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState([]);
+  const [connectedClients, setConnectedClients] = useState(0);
+  const [userID, setuserID] = useState(null); 
+
+  const socketRef = useRef();
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT, {
+      transports: ["websocket"],
+      upgrade: true,
+    }); 
+    socket.on('yourID', (ID) => {
+      setuserID(ID);
+      console.log('User joined with ID: ' + ID);
+    });
+
+    socket.on("message", (data) => {
+      setChat(oldChat => [...oldChat, data]);
+    });
+
+    socket.on("connectedClients", (count) => {
+      setConnectedClients(count);
+    });
+
+    socketRef.current = socket;
+
+    return () => socket.disconnect();
+  }, []);
+
+  const sendMessage = () => {
+    socketRef.current.emit('message', message);  
+    setMessage("");
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Real-time chat</p>
+        <p>Connected users: {connectedClients}</p>
+        <div>
+          {chat.map((msg, index) => <div key={index}>{msg}</div>)}
+        </div>
+        <input value={message} onChange={(e) => setMessage(e.target.value)} />
+        <button onClick={sendMessage}>Send</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
