a8704f209d3cf7a36a026257ae9c21b226085702
regraced <lukee249@outlook.com>
2023-09-28 23:42:43 -0500

    small patch

--- node/index.js
+++ node/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const Redis = require("ioredis");
-import { useNavigate } from "react-router-dom";
 
 const app = express();
 const server = http.createServer(app);
